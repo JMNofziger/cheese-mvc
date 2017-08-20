77e72c45a1e7b3813863d95223175766f48d0731
jmnofziger <jacob.nofziger@gmail.com>
2017-08-20 12:33:29 -0700

    Made cheeses arraylist global, modified controller to allow user to add new cheeses via /add url, modified templates to make that process simple

--- src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
+++ src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
@@ -1,8 +1,11 @@
 package work.jmnofziger.cheesemvc.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -10,29 +13,28 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     // Request path:/cheese
     @RequestMapping(value="")
     public String index(Model model){
         String title = "My Cheeses";
-
-        ArrayList<String> cheeses = new ArrayList<>();
-        cheeses.add("cheddar");
-        cheeses.add("parmesan");
-        cheeses.add("munster");
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title",title);
         return "cheese/index";
     }
 
-    @RequestMapping(value="add")
+    @RequestMapping(value="add", method=RequestMethod.GET)
     public String displayAddCheeseForm(Model model){
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
     }
 
-    @RequestMapping(value="add")
-    public String processAddCheeseForm(){
-
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    // Rewritten with RequestParam annotation to replace HttpServletRequest
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        return "redirect:";
     }
 }
