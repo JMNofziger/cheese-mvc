4418d61ffcc5c1bacf993e8708890a4be7e2207b
jmnofziger <jacob.nofziger@gmail.com>
2017-08-20 00:06:08 -0700

    Added skeleton for todo list to controller and thymeleaf template

--- src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
+++ src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
@@ -12,6 +14,13 @@ public class CheeseController {
     @RequestMapping(value="")
     public String index(Model model){
         String title = "My Cheeses";
+
+        ArrayList<String> cheeses = new ArrayList<>();
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("munster");
+
+//        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title",title);
         return "cheese/index";
     }
