e14e2d48cb65da94014ecb8ae03104ba82ae5c9b
jmnofziger <jacob.nofziger@gmail.com>
2017-08-20 16:22:37 -0700

    Refactored ArrayList object to HashMap to hold cheese and description, modified templates to take user desc. of cheese and output to view.

--- src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
+++ src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String,String> cheeses = new HashMap<>();
 
     // Request path:/cheese
     @RequestMapping(value="")
@@ -31,8 +32,8 @@ public class CheeseController {
 
     @RequestMapping(value="add", method=RequestMethod.POST)
     // Rewritten with RequestParam annotation to replace HttpServletRequest
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+        cheeses.put(cheeseName,cheeseDescription);
         return "redirect:";
     }
 }
