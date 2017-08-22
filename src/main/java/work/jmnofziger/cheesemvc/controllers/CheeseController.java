2faeb71a5766feeee248732d6e0c3a7e52f2b2d6
jmnofziger <jacob.nofziger@gmail.com>
2017-08-21 19:48:38 -0700

    Added removal form to view template and handling controller for removal of items

--- src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
+++ src/main/java/work/jmnofziger/cheesemvc/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package work.jmnofziger.cheesemvc.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("cheese")
@@ -16,12 +18,24 @@ public class CheeseController {
     static HashMap<String,String> cheeses = new HashMap<>();
 
     // Request path:/cheese
-    @RequestMapping(value="")
+    @RequestMapping(value="",method=RequestMethod.GET)
     public String index(Model model){
         String title = "My Cheeses";
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title",title);
         return "cheese/index";
+    }
+
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(HttpServletRequest request){
+        //Process request to get all items from removal form
+        List<String> parameterNames = new ArrayList<>(request.getParameterMap().keySet());
+
+        //Remove items from cheese list
+        for(String cheese : parameterNames){
+            cheeses.remove(cheese);
+        }
+        return "redirect:";
     }
 
     @RequestMapping(value="add", method=RequestMethod.GET)
